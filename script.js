21534892712d51a784893a4cf7b1836e7803b758
Turma Generation <raullalvarenga@hotmail.com>
2019-11-28 16:43:23 -0300

    fim da aula

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-var nome = prompt("Digite o seu nome: ");
-var altu = Number(prompt("Digite sua altura em cm: ")) / 100;
-var peso = parseFloat(prompt("Digite seu peso em Kg: "));
-var clientImc = imc(altu, peso).toFixed(2);
+// var nome = prompt("Digite o seu nome: ");
+// var altu = Number(prompt("Digite sua altura em cm: ")) / 100;
+// var peso = parseFloat(prompt("Digite seu peso em Kg: "));
+// var clientImc = imc(altu, peso).toFixed(2);
 
-function imc(a, p) {
-    return parseFloat(p / Math.pow(a, 2));
-}
+// function imc(a, p) {
+//     return parseFloat(p / Math.pow(a, 2));
+// }
 
-function defImc(imc) {
-    if (imc < 16)
-        return "Peso muito Baixo";
-    else if (imc < 17)
-        return "Peso baixo - grave";
-    else if (imc < 18, 50)
-        return "Peso baixo.";
-    else if (imc < 25)
-        return "Peso normal";
-    else if (imc < 30)
-        return "Sobrepeso";
-    else if (35)
-        return "Obesidade grau ¹";
-    else if (imc < 40)
-        return "Obesidade grau ²";
-    else if (imc >= 40)
-        return "Obesidade grau ³";
-    else
-        return "imc inválido";
-}
+// function defImc(imc) {
+//     if (imc < 16)
+//         return "Peso muito Baixo";
+//     else if (imc < 17)
+//         return "Peso baixo - grave";
+//     else if (imc < 18, 50)
+//         return "Peso baixo.";
+//     else if (imc < 25)
+//         return "Peso normal";
+//     else if (imc < 30)
+//         return "Sobrepeso";
+//     else if (35)
+//         return "Obesidade grau ¹";
+//     else if (imc < 40)
+//         return "Obesidade grau ²";
+//     else if (imc >= 40)
+//         return "Obesidade grau ³";
+//     else
+//         return "imc inválido";
+// }
 
-function click1() {
-    alert((nome + " possui indíce de massa corporal igual a " + clientImc + ", sendo classificado como " + defImc(imc)));
-}
+// function click1() {
+//     alert((nome + " possui indíce de massa corporal igual a " + clientImc + ", sendo classificado como " + defImc(imc)));
+// }
+
+d = new Date
+alert(Date.UTC().);
--- testes/sketch.js
+++ testes/sketch.js
@@ -1,72 +1,72 @@
 let system;
 
 function setup() {
-    createCanvas(640, 800);
-    // system = new ParticleSystem(createVector(width / 2, 50));
+    createCanvas(1366, 800);
+    system = new ParticleSystem(createVector(250, 50));
 }
 
 function draw() {
     {
-        ellipse(mouseX, mouseY, 12, 12);
-        if (mouseIsPressed)
-            fill(0);
-        else
-            fill(255);
-        noStroke();
+        // ellipse(mouseX, mouseY, 12, 12);
+        // if (mouseIsPressed)
+        //     fill(0);
+        // else
+        //     fill(255);
+        // noStroke();
     }
-    // background(51);
-    // system.addParticle();
-    // system.run();
+    background(51);
+    system.addParticle();
+    system.run();
 }
 
-// let Particle = function (position) {
-//     this.acceleration = createVector(0, 0.05);
-//     this.velocity = createVector(random(-1, 1), random(-1, 0));
-//     this.position = position.copy();
-//     this.lifespan = 300;
-// };
+let Particle = function (position) {
+    this.acceleration = createVector(0, 0.1);
+    this.velocity = createVector(random(-3, 3), random(-3, 0));
+    this.position = position.copy();
+    this.lifespan = 300;
+};
 
-// Particle.prototype.run = function() {
-//     this.update();
-//     this.display();
-//   };
+Particle.prototype.run = function() {
+    this.update();
+    this.display();
+  };
   
-//   // Method to update position
-//   Particle.prototype.update = function(){
-//     this.velocity.add(this.acceleration);
-//     this.position.add(this.velocity);
-//     this.lifespan -= 2;
-//   };
+  // Method to update position
+  Particle.prototype.update = function(){
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.lifespan -= 2;
+  };
   
-//   // Method to display
-//   Particle.prototype.display = function() {
-//     stroke(200, this.lifespan);
-//     strokeWeight(2);
-//     fill(300, this.lifespan);
-//     ellipse(this.position.x, this.position.y, 12, 12);
-//   };
+  // Method to display
+  Particle.prototype.display = function() {
+    stroke(200, this.lifespan);
+    strokeWeight(2);
+    fill(300, this.lifespan);
+    ellipse(this.position.x, this.position.y, 12, 12);
+  };
   
-//   // Is the particle still useful?
-//   Particle.prototype.isDead = function(){
-//     return this.lifespan < 0;
-//   };
+  // Is the particle still useful?
+  Particle.prototype.isDead = function(){
+    return this.lifespan < 0;
+  };
   
-//   let ParticleSystem = function(position) {
-//     this.origin = position.copy();
-//     this.particles = [];
-//   };
+  let ParticleSystem = function(position) {
+    this.origin = position.copy();
+    this.particles = [];
+  };
   
-//   ParticleSystem.prototype.addParticle = function() {
-//     this.particles.push(new Particle(this.origin));
-//   };
+  ParticleSystem.prototype.addParticle = function() {
+    this.particles.push(new Particle(this.origin));
+  };
   
-//   ParticleSystem.prototype.run = function() {
-//     for (let i = this.particles.length-1; i >= 0; i--) {
-//       let p = this.particles[i];
-//       p.run();
-//       if (p.isDead()) {
-//         this.particles.splice(i, 1);
-//       }
-//     }
-//   };
+  ParticleSystem.prototype.run = function() {
+    for (let i = this.particles.length-1; i >= 0; i--) {
+      let p = this.particles[i];
+      p.run();
+      if (p.isDead()) {
+        this.particles.splice(i, 1);
+      }
+    }
+  };
   
